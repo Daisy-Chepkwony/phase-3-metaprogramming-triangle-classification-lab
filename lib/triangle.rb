e9823f8e594c68a9dfdc70e0302ab39da7010f99
+ <+>
2023-02-13 16:09:59 +0300

    commit tree cbc1dfcce40ea12e2634d8de8607cff339b5f537

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError; end
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    if [a, b, c].min <= 0
+      raise TriangleError, "Sides must be greater than 0"
+    end
+    if a + b <= c || a + c <= b || b + c <= a
+      raise TriangleError, "The sum of the lengths of any two sides must be greater than the length of the third side"
+    end
+    @a = a
+    @b = b
+    @c = c
+    
+  end
+  
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
+
